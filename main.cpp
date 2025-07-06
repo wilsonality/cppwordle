51be56ec7c6b0927b35c59ec23200e80dfb4923f
wilsonality <ptcisbored@gmail.com>
2025-07-06 12:52:11 -0400

    inital commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include<words.txt>
+
+using namespace std;
+
+int main(int , char**){
+    cout << "welcome to the wordle game.";
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## hello!
+
+this is a rudimentary wordle game in the terminal, using the language C++.
+
+the program will
+- allow users to make six guesses
+- see the accuracy of their guesses
