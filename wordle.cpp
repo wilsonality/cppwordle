c7fb69c674bd576b60c598df84b9d5293a6d4e53
wilsonality <ptcisbored@gmail.com>
2025-07-07 02:05:04 -0400

    gameloop work

---
+++ wordle.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+
+int gameloop(){
+    int check_guess(string guess){
+        // 
+        return 0;
+    };
+
+    // get word length
+
+    int word_length;
+    cout << "Please select a word length";
+    cin >> word_length;
+    cout << "drawing a word of length " << word_length <<"...";
+
+    // draw a word from word list
+
+    string solution;
+
+    int guess_ct = 0;
+
+    while (guess_ct < 6){
+        string cur_guess;
+        check_guess(cur_guess);
+    }
+
+    return 0;
+
+
+
+    
+};
+
